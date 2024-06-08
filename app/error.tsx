8a12467c2333e2cf2c3bf27e432d87c958d6b984
isderific <164430536+isderific@users.noreply.github.com>
2024-06-09 04:28:58 +0700

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -5,21 +5,15 @@ import Center from "@/components/layouts/center";
 import MessageBox from "@/components/message-box";
 import { useEffect } from "react";
 
-export default function Error({
-    error,
-    reset,
-}: {
-    error: Error & { digest?: string };
-    reset: () => void;
-}) {
-    useEffect(() => {
-        console.error(error);
-    }, [error]);
-    ({
+import CardList from "@/components/card-list";
+import { DEFAULT_PER_PAGE } from "@/lib/constants";
+import SearchCardList from "@/components/search/search-list";
+import { SITENAME } from "@/lib/constants";
+export default async function Home({
     searchParams,
 }: {
-    searchParams: { [key: string]: string | string[] | undefined }
-}); =>
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
     const page =
         (searchParams.page && parseInt(searchParams.page as string)) || 10;
     const per_page =
@@ -30,10 +24,7 @@ export default function Error({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <MessageBox title={error.message} countdown={3}>
-            <Button onClick={reset}>Try Again</Button>
-        </MessageBox>
-        <div className="md:my-2">
+        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2>
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
