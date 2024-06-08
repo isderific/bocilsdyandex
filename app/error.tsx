4c43cbde835cb57285c33986dd739e2c33201fd7
isderific <164430536+isderific@users.noreply.github.com>
2024-06-09 04:22:20 +0700

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -15,10 +15,30 @@ export default function Error({
     useEffect(() => {
         console.error(error);
     }, [error]);
+    ({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    const page =
+        (searchParams.page && parseInt(searchParams.page as string)) || 10;
+    const per_page =
+        (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
+        DEFAULT_PER_PAGE;
+    const fld_id =
+        (searchParams.fld_id && (searchParams.fld_id as string)) || undefined;
+    const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
         <MessageBox title={error.message} countdown={3}>
             <Button onClick={reset}>Try Again</Button>
         </MessageBox>
+        <div className="md:my-2">
+            {query ? (
+                <SearchCardList query={query} banner />
+            ) : (
+                <CardList page={page} per_page={per_page} fld_id={fld_id} />
+            )}
+        </div>
     );
 }
