b716dbb551055964a6522ac3c14ee2caf320d153
isderific <164430536+isderific@users.noreply.github.com>
2025-02-22 03:22:43 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import "./globals.css";
-import Script from "next/script";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: `Bocil Sd Yandex - Video Bokep Indo Viral Terbaru`,
-    description: `Bocil Sd Yandex Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
     metadataBase: new URL("https://bocilsdyandex.pages.dev"),
     alternates: {
         canonical: `/`,
     },
     openGraph: {
-        title: `Bocil Sd Yandex - Video Bokep Indo Jepang Barat Viral Terbaru`,
-        description: `Bocil Sd Yandex Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         url: `/`,
         type: `website`,
     },
@@ -35,8 +35,8 @@ export default function RootLayout({
     const jsonLd = {
         "@context": "https://schema.org",
         "@type": "Organization",
-        "name": `Bocil Sd Yandex`,
-        "description": `Bocil Sd Yandex Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         "logo": "https://bocilsdyandex.pages.dev/favicon.ico",
         "url": "https://bocilsdyandex.pages.dev",
         "founder":{
@@ -44,25 +44,14 @@ export default function RootLayout({
             "name":"admin",
             "url":"https://bocilsdyandex.pages.dev"
             },
-            "foundingDate":"2024-06-09"
-        }
-        const jsonLd1 = {
-            "@context": "https://schema.org",
-        "@type": "WebPage",
-        "name": `Bocil Sd Yandex`,
-        "url": "https://bocilsdyandex.pages.dev",
-        "description": `Bocil Sd Yandex Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
-        "image": "https://bocilsdyandex.pages.dev/favicon.ico",
-        "potentialAction":{
-            "@type":"ReadAction",
-            "target":"https://bocilsdyandex.pages.dev"}
+            "foundingDate":"2025-02-20"
         }
         const jsonLd2 = {
             "@context": "https://schema.org",
         "@type": "WebSite",
-        "name": `Bocil Sd Yandex`,
+        "name": `${SITENAME}`,
         "url": "https://bocilsdyandex.pages.dev",
-        "description": `Bocil Sd Yandex Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         "potentialAction": { 
         "@type": "SearchAction", 
           "target": "https://bocilsdyandex.pages.dev/?q={search_term}", 
@@ -83,16 +72,11 @@ export default function RootLayout({
         />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
-        />
-        <script
-          type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
         />
                     {children}
                 </ThemeProvider>
             </body>
-<Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w2e4z2e4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
         </html>
     );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,32 +1,5 @@
 import MessageBox from "@/components/message-box";
-import CardList from "@/components/card-list";
-import { DEFAULT_PER_PAGE } from "@/lib/constants";
-import SearchCardList from "@/components/search/search-list";
-import { SITENAME } from "@/lib/constants";
-export async function Home({
-    searchParams,
-}: {
-    searchParams: { [key: string]: string | string[] | undefined };
-}) {
-    const page =
-        (searchParams.page && parseInt(searchParams.page as string)) || 10;
-    const per_page =
-        (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
-        DEFAULT_PER_PAGE;
-    const fld_id =
-        (searchParams.fld_id && (searchParams.fld_id as string)) || undefined;
-    const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
-    return (
-        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2>
-            {query ? (
-                <SearchCardList query={query} banner />
-            ) : (
-                <CardList page={page} per_page={per_page} fld_id={fld_id} />
-            )}
-        </div>
-    );
-}
 export default function NotFound() {
     return (
         <MessageBox title="You are lost!" countdown={3}>
